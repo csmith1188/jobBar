6e11c99ce9d4c66ecaedebd9c9cc489e9777b4f8
Carlos Ort-Patrick <27COrtPatrick@live.ytech.edu>
2026-01-30 14:47:32 -0500

    almost done, just requires some fixs and connections to the page that gabby is making

--- socket/pay.js
+++ socket/pay.js
@@ -1,4 +1,7 @@
 const io = require('socket.io-client');
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
 
 const socket = io("https://formbeta.yorktechapps.com/", {
     extraHeaders: {
@@ -30,3 +33,40 @@ socket.on('connect_error', (err) => {
     console.error('Connection Error:', err.message);
 });
 
+router.post('/api/digipogs/transfer', (req, res) => {
+    // req.body gets the information sent from the client
+    const cost = req.body.price;
+    const payload = req.body;
+    const reason = payload.reason;
+    const pin = payload.pin;
+    const id = req.session.user.fid; // Formbar user ID of payer from session
+   
+   
+    console.log(cost, reason, pin, id);
+    const paydesc = {
+        from: id, // Formbar user ID of payer
+        to: 30,    // Formbar user ID of payee (e.g., pog collecting's account)
+        amount: cost,
+        reason: reason,
+        // security pin for the payer's account
+        pin: pin,
+        pool: true
+    };
+    // make a direct transfer request using fetch
+    fetch(`${AUTH_URL}/api/digipogs/transfer`, {
+        method: 'POST',
+        // headers to specify json content
+        headers: { 'Content-Type': 'application/json' },
+        // stringify the paydesc object to send as JSON
+        body: JSON.stringify(paydesc),
+    }).then((transferResult) => {
+        return transferResult.json();
+    }).then((responseData) => {
+        console.log("Transfer Response:", responseData);
+        //res.JSON must be here to send the response back to the client
+        res.json(responseData);
+    }).catch(err => {
+        console.error("Error during digipog transfer:", err);
+        res.status(500).json({ message: 'Error during digipog transfer' });
+    });
+});
